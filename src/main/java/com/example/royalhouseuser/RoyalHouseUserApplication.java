272b3eb8b189fdf283cae380c1188aedfede5e65
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-05-08 19:21:03 +0300

    initialize a project

---
+++ src/main/java/com/example/royalhouseuser/RoyalHouseUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.royalhouseuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoyalHouseUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoyalHouseUserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/royalhouseuser/RoyalHouseUserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.royalhouseuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoyalHouseUserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
