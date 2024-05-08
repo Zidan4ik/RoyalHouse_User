3454da15f83c729bda27942871727c356a41b30b
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-05-08 20:28:43 +0300

    vuexy files

--- src/main/java/com/example/royalhouseuser/RoyalHouseUserApplication.java
+++ src/main/java/com/example/royalhouseuser/RoyalHouseUserApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RoyalHouseUserApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RoyalHouseUserApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RoyalHouseUserApplication.class, args);
+    }
 
 }
